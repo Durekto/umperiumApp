577ee40b8aea4c4efdb9d8a6b89c22dc9fdade8c
alexanderkushnirchuk@gmail.com <alexanderkushnirchuk@gmail.com>
2020-03-24 22:06:03 +0200

    login(not done yet)

--- react-ionic/src/pages/user/Login.tsx
+++ react-ionic/src/pages/user/Login.tsx
@@ -1,39 +1,47 @@
 import React from 'react';
 
 import './Login.css';
-import { IonPage, IonContent, IonImg, IonInput, IonCheckbox, IonButton, IonItem, IonHeader, IonLabel, IonToolbar, IonTitle } from '@ionic/react';
-
-//#region Images
-
-// import ump from '/login/logo-black-title.png';
-// import vk from '/login/vk.png';
-// import fb from '/login/f.png';
-
-//#endregion
+import { 
+    IonPage, 
+    IonContent, 
+    IonImg, 
+    IonInput, 
+    IonCheckbox, 
+    IonButton, 
+    IonItem, 
+    IonLabel 
+} from '@ionic/react';
 
 function Login() {
     return(
         <IonPage>
-            <IonContent class='_login'>
-                <div className='card-body'>
-                    <div className='text-center'>
-                        <a href='#'>
-                            <IonImg src='/login/logo-black-title.png' alt='logo' class='_login-logo'></IonImg>
-                        </a>
-                    </div>
+            <IonContent className='_login'>
+                <div className='card'>
+                    <div className='card-body'>
+                        <div className='text-center'>
+                            <a href='#'>
+                                <IonImg src='/login/logo-black-title.png' alt='logo' className='_login-logo'></IonImg>
+                            </a>
+                        </div>
 
-                    <IonInput class='form-control' type='email' placeholder='E-mail'></IonInput>
-                    <IonInput class='form-control' type='password' placeholder='Пароль'></IonInput>
-                    <IonLabel class='custom-control-label'><IonCheckbox class='custom-control'></IonCheckbox>Запомнить меня</IonLabel>
-                    <IonButton type='submit'>Войти</IonButton>
-                    <IonItem>
-                        <IonImg src='/login/vk.png' alt='vk' class='_login-social-network'></IonImg>
-                        <a href='#/vk'>Войти через ВК</a>
-                    </IonItem>
-                    <IonItem>
-                        <IonImg src='/login/f.png' alt='fb' class='_login-social-network'></IonImg>
-                        <a href='#/fb'>Войти через FB</a>
-                    </IonItem>
+                        <form className='form-horizontal' action='' method='post'>
+                            <IonInput className='form-control' type='email' placeholder='E-mail'></IonInput>
+                            <IonInput className='form-control' type='password' placeholder='Пароль'></IonInput>
+                            <IonItem className='ion-item-highlight' lines='none'>
+                                <IonLabel >Запомнить меня</IonLabel>
+                                <IonCheckbox slot='start' value='isRemembered'></IonCheckbox>
+                            </IonItem>
+                            <IonButton className='login-button' type='submit'>Войти</IonButton>
+                            <IonItem className='ion-item-highlight' lines='none' target='_blank' href='#vk'>
+                                <IonImg src='/login/vk.png' alt='vk' className='_login-social-network'></IonImg>
+                                <IonLabel>Войти через ВК</IonLabel>
+                            </IonItem>
+                            <IonItem className='ion-item-highlight' lines='none' target='_blank' href='#fb'>
+                                <IonImg src='/login/f.png' alt='fb' className='_login-social-network'></IonImg>
+                                <IonLabel>Войти через FB</IonLabel>
+                            </IonItem>
+                        </form>
+                    </div>
                 </div>
             </IonContent>
         </IonPage>
