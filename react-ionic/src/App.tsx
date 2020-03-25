03fc94dc5df9894292fb14fdf19c05e362e7f1a6
alexanderkushnirchuk@gmail.com <alexanderkushnirchuk@gmail.com>
2020-03-25 03:23:45 +0200

    Fixes and improvements
    Tab Icons added;
    Minor changes to Login page;
    HeaderLogo fixed. Now it has fixed size and always at the center of the header;

--- react-ionic/src/App.tsx
+++ react-ionic/src/App.tsx
@@ -31,6 +31,7 @@ import {
     // IonCardSubtitle,
     // IonCardTitle,
     IonAvatar,
+    IonImg,
 
 
 } from '@ionic/react';
@@ -79,121 +80,114 @@ function App() {
       <IonApp>
         <IonHeader id='common-header'>
           <IonToolbar color='dark' className="wrapperHeader">
-              <div className="wrapperHeader">
-                  <div>
-            <IonButtons slot='start'>
-                <IonMenuButton menu='main-menu'></IonMenuButton>
-            </IonButtons>
-                  </div>
-                  <div>
-              <IonItem  button href='/home' color='dark'>
+            <div className="wrapperHeader">
+              <div>
+                <IonButtons slot='start'>
+                  <IonMenuButton menu='main-menu'></IonMenuButton>
+                </IonButtons>
+              </div>
+              <div>
+                <IonItem detail={false} button href='/home' color='dark'>
                   <img className="logo" src="/logo.png" alt=""/>
-              </IonItem>
-                  </div>
-
-                  <IonModal isOpen={showModal}>
-                      <IonSearchbar></IonSearchbar>
-                      <IonCard class="wrapperCard">
-                          <IonCardHeader className="wrapperForUserInfo">
-
-                              <IonList>
-                                  <IonItem>
-                                      <IonAvatar slot="start">
-                                          <img src="./user.jpg" alt=""/>
-                                      </IonAvatar>
-                                      <IonLabel>User name</IonLabel>
-                                      <img className="userCountry" src="./RUS.png" alt=""/>
-                                      <img className="gem" src="./gem.svg" alt=""/>
-                                  </IonItem>
-                              </IonList>
-
-                          </IonCardHeader>
-
-                          <IonCardContent>
-                              Keep close to Nature's heart... and break clear away, once in awhile,
-                              and climb a mountain or spend a week in the woods. Wash your spirit clean.
-                          </IonCardContent>
-                      </IonCard>
-                      <IonButton onClick={() => setShowModal(false)}>Close Search</IonButton>
-                  </IonModal>
-                  <div>
-                  <IonButton className="btnSearch" slot='end' onClick={() => setShowModal(true)}>
-                      <img src="./whiteSearch.png" alt=""/>
-                  </IonButton>
-                  </div>
+                </IonItem>
               </div>
-              <div className="wrapperDownHeaderMenu">
-                  <span>Популярное</span>
-                  <span>Обсуждение</span>
-                  <span>Сила Мысли</span>
-                  <span>Новое</span>
-              </div>
+              <IonModal isOpen={showModal}>
+                <IonSearchbar></IonSearchbar>
+                <IonCard class="wrapperCard">
+                  <IonCardHeader className="wrapperForUserInfo">
+                    <IonList>
+                        <IonItem>
+                            <IonAvatar slot="start">
+                                <img src="./user.jpg" alt=""/>
+                            </IonAvatar>
+                            <IonLabel>User name</IonLabel>
+                            <img className="userCountry" src="./RUS.png" alt=""/>
+                            <img className="gem" src="./gem.svg" alt=""/>
+                        </IonItem>
+                    </IonList>
+                  </IonCardHeader>
+                  <IonCardContent>
+                      Keep close to Nature's heart... and break clear away, once in awhile,
+                      and climb a mountain or spend a week in the woods. Wash your spirit clean.
+                  </IonCardContent>
+                </IonCard>
+                <IonButton onClick={() => setShowModal(false)}>Close Search</IonButton>
+              </IonModal>
+              {/* <div> */}
+              <IonButton className="btnSearch" slot='end' onClick={() => setShowModal(true)}>
+                  <IonImg src='./whiteSearch.png' alt='search'></IonImg>
+              </IonButton>
+              {/* </div> */}
+            </div>
+            <div className="wrapperDownHeaderMenu">
+                <span>Популярное</span>
+                <span>Обсуждение</span>
+                <span>Сила Мысли</span>
+                <span>Новое</span>
+            </div>
           </IonToolbar>
-
         </IonHeader>
         <IonNav></IonNav>
 
-        {/* <IonSplitPane> */}
-          <IonMenu menuId='main-menu' contentId='menu-items' type='overlay'>
-            <IonHeader className="userInfo">
-                <div>
-                <img className="BgImgUserMenu" src="/imgBG5.jpg" alt=""/>
-                <div className="wrapperUserImgName">
-                    <img className="userAvatar" src="/user.jpg" alt=""/>
-                    <span>User name</span>
-                </div>
-                </div>
-              <IonToolbar>
-                <IonTitle>Menu</IonTitle>
-              </IonToolbar>
-            </IonHeader>
-            <IonContent id='menu-items'>
-              <IonList>
-                <IonMenuToggle menu='main-menu'>
-                  {/*<IonItem href='/home'>*/}
-                  {/*  <IonIcon icon={home} slot="start"></IonIcon>*/}
-                  {/*  <IonLabel>Моя страница</IonLabel>*/}
-                  {/*</IonItem>*/}
-                  <IonItem href='/profile'>
-                    <IonIcon icon={person} slot="start"></IonIcon>
-                    <IonLabel>Счет</IonLabel>
-                  </IonItem>
-                  <IonItem href='/chat'>
-                    <IonIcon icon={chatbubbles} slot="start"></IonIcon>
-                    <IonLabel>Настройки</IonLabel>
-                  </IonItem>
-                  <IonItem href='/settings'>
-                    <IonIcon icon={settings} slot="start"></IonIcon>
-                    <IonLabel>Лента</IonLabel>
-                  </IonItem>
-
-                    <div className="hrBorder"></div>
-
-                  <IonItem href='/settings'>
-                    <IonIcon icon={settings} slot="start"></IonIcon>
-                    <IonLabel>Центр монетизации</IonLabel>
-                  </IonItem>
-                    <IonItem href='/settings'>
-                    <IonIcon icon={settings} slot="start"></IonIcon>
-                    <IonLabel>Служба поддержки</IonLabel>
-                  </IonItem>
-                    <IonItem href='/settings'>
-                    <IonIcon icon={settings} slot="start"></IonIcon>
-                    <IonLabel>Вопросы - ответы</IonLabel>
-                  </IonItem>
-                    <IonItem href='/settings'>
-                    <IonIcon icon={settings} slot="start"></IonIcon>
-                    <IonLabel>Лидеры мнений</IonLabel>
+        <IonMenu menuId='main-menu' contentId='menu-items' type='overlay'>
+          <IonHeader className="userInfo">
+              <div>
+              <img className="BgImgUserMenu" src="/imgBG5.jpg" alt=""/>
+              <div className="wrapperUserImgName">
+                  <img className="userAvatar" src="/user.jpg" alt=""/>
+                  <span>User name</span>
+              </div>
+              </div>
+            <IonToolbar>
+              <IonTitle>Menu</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent id='menu-items'>
+            <IonList>
+              <IonMenuToggle menu='main-menu'>
+                {/* <IonItem href='/auth'>
+                  <IonIcon icon={person} slot="start"></IonIcon>
+                  <IonLabel>Вход/Регистрация</IonLabel>
+                </IonItem> */}
+                <IonItem href='/profile'>
+                  <IonIcon icon={person} slot="start"></IonIcon>
+                  <IonLabel>Счет</IonLabel>
                 </IonItem>
-                    <IonItem href='/settings'>
-                    <IonIcon icon={settings} slot="start"></IonIcon>
-                    <IonLabel>Элита</IonLabel>
-                  </IonItem>
-                </IonMenuToggle>
-              </IonList>
-            </IonContent>
-          </IonMenu>
-        {/* </IonSplitPane> */}
+                <IonItem href='/chat'>
+                  <IonIcon icon={chatbubbles} slot="start"></IonIcon>
+                  <IonLabel>Настройки</IonLabel>
+                </IonItem>
+                <IonItem href='/settings'>
+                  <IonIcon icon={settings} slot="start"></IonIcon>
+                  <IonLabel>Лента</IonLabel>
+                </IonItem>
+
+                  <div className="hrBorder"></div>
+
+                <IonItem href='/settings'>
+                  <IonIcon icon={settings} slot="start"></IonIcon>
+                  <IonLabel>Центр монетизации</IonLabel>
+                </IonItem>
+                  <IonItem href='/settings'>
+                  <IonIcon icon={settings} slot="start"></IonIcon>
+                  <IonLabel>Служба поддержки</IonLabel>
+                </IonItem>
+                <IonItem href='/settings'>
+                  <IonIcon icon={settings} slot="start"></IonIcon>
+                  <IonLabel>Вопросы - ответы</IonLabel>
+                </IonItem>
+                  <IonItem href='/settings'>
+                  <IonIcon icon={settings} slot="start"></IonIcon>
+                  <IonLabel>Лидеры мнений</IonLabel>
+                </IonItem>
+                <IonItem href='/settings'>
+                  <IonIcon icon={settings} slot="start"></IonIcon>
+                  <IonLabel>Элита</IonLabel>
+                </IonItem>
+              </IonMenuToggle>
+            </IonList>
+          </IonContent>
+        </IonMenu>
 
         <IonReactRouter>
           {/* <IonRouterOutlet>
@@ -203,34 +197,37 @@ function App() {
           </IonRouterOutlet> */}
 
           <IonTabs>
-              <IonRouterOutlet>
-                  <Route path="/tab1" component={Tab1} exact={true} />
-                  <Route path="/tab2" component={Tab1} exact={true} />
-                  <Route path="/tab3" component={Tab1} exact={true} />
-                  <Route path="/tab4" component={Tab1} exact={true} />
-                  <Route path='/auth' component={Login} exact={true} />
-                  <Route path="/home" component={Home} exact={true} />
-                  <Route path="/" render={() => <Redirect to="/home" />} exact={true} />
-              </IonRouterOutlet>
+            <IonRouterOutlet>
+              <Route path="/home" component={Home} exact={true} />
+              <Route path="/tab2" component={Tab1} exact={true} />
+              <Route path="/tab3" component={Tab1} exact={true} />
+              <Route path="/profile" component={Tab1} exact={true} />
+              <Route path='/auth' component={Login} exact={true} />
+              <Route path="/" render={() => <Redirect to="/home" />} exact={true} />
+            </IonRouterOutlet>
 
-              <IonTabBar slot="bottom">
-                  <IonTabButton tab="tab1" href="/tab1">
-                      <IonIcon icon={home} />
-                      <IonLabel>Главная</IonLabel>
-                  </IonTabButton>
-                  <IonTabButton tab="tab2" href="/tab2">
-                      <IonIcon icon={settings} />
-                      <IonLabel>Написать пост</IonLabel>
-                  </IonTabButton>
-                  <IonTabButton tab="tab3" href="/tab3">
-                      <IonIcon icon={informationCircle} />
-                      <IonLabel>Моя страница</IonLabel>
-                  </IonTabButton>
-                  <IonTabButton tab="about" href="/tab4">
-                      <IonIcon icon={informationCircle} />
-                      <IonLabel>Магазин влияния</IonLabel>
-                  </IonTabButton>
-              </IonTabBar>
+            <IonTabBar slot="bottom" color='dark'>
+              <IonTabButton tab="home" href="/home">
+                <IonImg className='tab-icons' src="/tab-icons/main.png"></IonImg>
+                  {/* <IonIcon src="/tab-icons/main.png" /> */}
+                  {/* <IonLabel>Главная</IonLabel> */}
+              </IonTabButton>
+              <IonTabButton tab="publishing" href="/publishing">
+                <IonImg className='tab-icons' src="/tab-icons/writing.png"></IonImg>
+                  {/* <IonIcon src="/tab-icons/writing.png" /> */}
+                  {/* <IonLabel>Написать пост</IonLabel> */}
+              </IonTabButton>
+              <IonTabButton tab="market" href="/market">
+                <IonImg className='tab-icons' src="/tab-icons/market.png"></IonImg>
+                  {/* <IonIcon src="/tab-icons/market.png" /> */}
+                  {/* <IonLabel>Моя страница</IonLabel> */}
+              </IonTabButton>
+              <IonTabButton tab="my-page" href="/auth">
+                <IonImg className='tab-icons' src="/tab-icons/my_page.png"></IonImg>
+                  {/* <IonIcon src="/tab-icons/my_page.png" /> */}
+                  {/* <IonLabel>Магазин влияния</IonLabel> */}
+              </IonTabButton>
+            </IonTabBar>
           </IonTabs>
         </IonReactRouter>
       </IonApp>
--- react-ionic/src/pages/user/Login.tsx
+++ react-ionic/src/pages/user/Login.tsx
@@ -16,44 +16,45 @@ import {
 function Login() {
     return(
         <IonPage>
-            <IonContent className='_login'>
+            <IonContent>
                 <div className="gap"></div>
+                <div className='_login'>
+                    <div className='card'>
+                        <div className='card-body'>
+                            <div className='text-center'>
+                                <a href='/home'>
+                                    <IonImg src='/login/logo-black-title.png' alt='logo' className='_login-logo'></IonImg>
+                                </a>
+                            </div>
 
-                <div className='card'>
-                    <div className='card-body'>
-                        <div className='text-center'>
-                            <a href='/home'>
-                                <IonImg src='/login/logo-black-title.png' alt='logo' className='_login-logo'></IonImg>
-                            </a>
+                            <form className='form-horizontal' action='' method='post'>
+                                <IonInput tabIndex={1} className='form-control' type='email' placeholder='E-mail'></IonInput>
+                                <IonInput tabIndex={2} className='form-control' type='password' placeholder='Пароль'></IonInput>
+                                <IonItem className='ion-item-highlight' lines='none'>
+                                    <IonCheckbox tabIndex={3} slot='start' value='isRemembered'></IonCheckbox>
+                                    <IonLabel>Запомнить меня</IonLabel>
+                                </IonItem>
+                                <IonButton tabIndex={4} className='login-button' type='submit'>Войти</IonButton>
+                                {/*  */}
+                                <IonItem tabIndex={5} className='ion-item-highlight' lines='none' target='_blank' href='#vk'>
+                                    <IonImg src='/login/vk.png' alt='vk' className='_login-social-network'></IonImg>
+                                    <IonLabel>Войти через ВК</IonLabel>
+                                </IonItem>
+                                <IonItem tabIndex={6} className='ion-item-highlight' lines='none' target='_blank' href='#fb'>
+                                    <IonImg src='/login/f.png' alt='fb' className='_login-social-network'></IonImg>
+                                    <IonLabel>Войти через FB</IonLabel>
+                                </IonItem>
+                                {/*  */}
+                                <IonItem tabIndex={5} className='ion-item-highlight' lines='none' target='_blank' href='#vk'>
+                                    <i className="fas fa-lock text-muted"></i>
+                                    <IonLabel>Проблемы со входом?</IonLabel>
+                                </IonItem>
+                                <IonItem tabIndex={6} className='ion-item-highlight' lines='none' target='_blank' href='#fb'>
+                                    <i className="fas fa-user-circle text-muted"></i>
+                                    <IonLabel>Регистрация</IonLabel>
+                                </IonItem>
+                            </form>
                         </div>
-
-                        <form className='form-horizontal' action='' method='post'>
-                            <IonInput tabIndex={1} className='form-control' type='email' placeholder='E-mail'></IonInput>
-                            <IonInput tabIndex={2} className='form-control' type='password' placeholder='Пароль'></IonInput>
-                            <IonItem className='ion-item-highlight' lines='none'>
-                                <IonCheckbox tabIndex={3} slot='start' value='isRemembered'></IonCheckbox>
-                                <IonLabel>Запомнить меня</IonLabel>
-                            </IonItem>
-                            <IonButton tabIndex={4} className='login-button' type='submit'>Войти</IonButton>
-                            {/*  */}
-                            <IonItem tabIndex={5} className='ion-item-highlight' lines='none' target='_blank' href='#vk'>
-                                <IonImg src='/login/vk.png' alt='vk' className='_login-social-network'></IonImg>
-                                <IonLabel>Войти через ВК</IonLabel>
-                            </IonItem>
-                            <IonItem tabIndex={6} className='ion-item-highlight' lines='none' target='_blank' href='#fb'>
-                                <IonImg src='/login/f.png' alt='fb' className='_login-social-network'></IonImg>
-                                <IonLabel>Войти через FB</IonLabel>
-                            </IonItem>
-                            {/*  */}
-                            <IonItem tabIndex={5} className='ion-item-highlight' lines='none' target='_blank' href='#vk'>
-                                <i className="fas fa-lock text-muted"></i>
-                                <IonLabel>Проблемы со входом?</IonLabel>
-                            </IonItem>
-                            <IonItem tabIndex={6} className='ion-item-highlight' lines='none' target='_blank' href='#fb'>
-                                <i className="fas fa-user-circle text-muted"></i>
-                                <IonLabel>Регистрация</IonLabel>
-                            </IonItem>
-                        </form>
                     </div>
                 </div>
             </IonContent>
